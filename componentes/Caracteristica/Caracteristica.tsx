ee2d16388aeab1e9afcee547f4b5d0e125fcd7dc
Gonçalo Lourenço a22106337 <128821979+sLx-Lourenco@users.noreply.github.com>
2025-11-22 19:44:08 +0000

    Commit

--- componentes/Caracteristica/Caracteristica.tsx
+++ componentes/Caracteristica/Caracteristica.tsx
@@ -3,8 +3,6 @@ export interface CaracteristicaProps {
 }
 
 export default function Caracteristica({ caracteristica }: CaracteristicaProps) {
-  const slug = caracteristica.toLowerCase().replace(/\s+/g, '-');
-
   return (
     <ul className="grid grid-cols-1 gap-4">
     <div className="flex flex-col items-center gap-4 p-4 rounded-xl shadow-md bg-white">
