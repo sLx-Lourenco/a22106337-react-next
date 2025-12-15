20a64482768caf39897abdf6411b17c6f90fe9ce
Gonçalo Lourenço a22106337 <128821979+sLx-Lourenco@users.noreply.github.com>
2025-12-15 00:10:39 +0000

    finalizado

--- app/deisiShop/categorias/[categoria]/page.tsx
+++ app/deisiShop/categorias/[categoria]/page.tsx
@@ -40,7 +40,7 @@ export default function CategoriaPage() {
         {params.categoria}
       </h1>
 
-      <div className="gap-6">
+      <div className="flex flex-col gap-6">
         {produtos.map(p => (
           <ProductBasicCard
             key={p.id}
